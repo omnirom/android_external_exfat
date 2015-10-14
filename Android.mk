85223bf48fcc962c8ad0bb105ee7438eb03b9de5
Ethan Yonker <dees_troy@teamw.in>
2015-10-14 08:27:55 -0500

    Remove recovery rules - TWRP does not need or want them
    TWRP has its own local exfat rules. These were carried over from
    forking from CM and the recovery rules should have been removed.
    
    Change-Id: Ib21c49b04ec26b8a145dbc2d6632d54486b2921d

--- Android.mk
+++ Android.mk
@@ -24,32 +24,6 @@ $(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
 
 ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
 
-
-# static multi-call binary for recovery
-include $(CLEAR_VARS)
-LOCAL_MODULE := mount.exfat_static
-LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
-LOCAL_MODULE_STEM := mount.exfat
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := main.c
-LOCAL_STATIC_LIBRARIES += libz libc
-LOCAL_STATIC_LIBRARIES += libexfat_mount libexfat_fsck libexfat_mkfs
-LOCAL_STATIC_LIBRARIES += libexfat libfuse
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-include $(BUILD_EXECUTABLE)
-
-RECOVERY_SYMLINKS := $(addprefix $(TARGET_RECOVERY_ROOT_OUT)/sbin/,$(LINKS))
-$(RECOVERY_SYMLINKS): EXFAT_BINARY := mount.exfat
-$(RECOVERY_SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
-	@echo "Symlink: $@ -> $(EXFAT_BINARY)"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf $(EXFAT_BINARY) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(RECOVERY_SYMLINKS)
-
-
 include $(FUSE_ROOT)/libexfat/Android.mk
 include $(FUSE_ROOT)/fuse/Android.mk
 include $(FUSE_ROOT)/mkfs/Android.mk
