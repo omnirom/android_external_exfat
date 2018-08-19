e7546a959337fd7f72a7f33db4c836070a6009fe
khanfrd <khanfrd94@gmail.com>
2018-08-20 01:16:31 +0530

    exfat: suppress warning
    Change-Id: I1bdb46905876a6dd97c5fadda46aa0e1341f176f

--- libexfat/Android.mk
+++ libexfat/Android.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libexfat
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64
-LOCAL_SRC_FILES = cluster.c io.c log.c lookup.c mount.c node.c time.c utf.c utils.c 
+LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64 -Wno-error -Wno-sign-compare -Wno-unused-parameter -Wno-address-of-packed-member -Wno-issing-field-initializers
+LOCAL_SRC_FILES = cluster.c io.c log.c lookup.c mount.c node.c time.c utf.c utils.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH) \
 LOCAL_SHARED_LIBRARIES += libc
 
