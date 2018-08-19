617e26d554e92d00aca095940e9a2044535b9cd4
khanfrd <khanfrd94@gmail.com>
2018-08-20 01:16:31 +0530

    exfat: suppress warning
    Change-Id: I1bdb46905876a6dd97c5fadda46aa0e1341f176f

--- fuse/Android.mk
+++ fuse/Android.mk
@@ -3,8 +3,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libexfat_mount
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64
-LOCAL_SRC_FILES = main.c 
+LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64 -Wno-unused-parameter
+LOCAL_SRC_FILES = main.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH) \
 					external/exfat/libexfat \
 					external/fuse/include \
